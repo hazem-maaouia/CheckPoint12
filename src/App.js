92c4dc0a3ad93cb23d034796743354192a49be4b
Hazem Maaouia <hazem.maaouia.lfi@gmail.com>
2021-01-30 22:05:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import myWonderfulImage from './imageInSrc.jpg';
 import './App.css';
+import './style.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+   <div style ={{border:" solid 1px black",maxWidth:"100vw"}}>
+ <h1 className="title red">Hazem Maaouia</h1>
+ <img src={myWonderfulImage} alt="imageinsrc"/>
+ <img src={"/imageInPublic.jpeg"} alt="imageinpublic"/>
+ </div>
+
+ <video width="320" height="240" controls >
+<source src={"/video.mkv"} type="video/mp4"/>
+</video>
+
+</div>
+
   );
 }
 
